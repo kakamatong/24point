199cc96785d27b2949bc07c98be9768520e80c34
kakamatong <631603920@qq.com>
2026-08-24 14:59:46 +0800

    feat: 移除CompBgCube组件中的ctrl_img控制器，简化构造逻辑

--- assets/scripts/fgui/lobbyBg/FGUICompBgCube.ts
+++ assets/scripts/fgui/lobbyBg/FGUICompBgCube.ts
@@ -8,7 +8,6 @@ import { Logger } from "@frameworks/utils/Utils";
 
 export default class FGUICompBgCube extends fgui.GComponent {
 
-	public ctrl_img:fgui.Controller;
 	public UI_TXT_NUM:fgui.GTextField;
 	public static URL:string = "ui://m52rjp09ddk1o";
 
@@ -73,7 +72,6 @@ export default class FGUICompBgCube extends fgui.GComponent {
 	}
 
 	protected onConstruct():void {
-		this.ctrl_img = this.getControllerAt(0);
 		this.UI_TXT_NUM = <fgui.GTextField>(this.getChildAt(1));
 		if (this.enableAnimation) this.enterAnimation();
 	}
