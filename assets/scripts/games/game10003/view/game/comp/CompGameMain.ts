e06a329e50876bbbf9a10c5c0512c7e4f4a73fe1
kakamatong <631603920@qq.com>
2026-09-14 20:14:22 +0800

    fix: 调整组件索引以修复解散房间按钮的事件处理

--- assets/scripts/games/game10003/view/game/comp/CompGameMain.ts
+++ assets/scripts/games/game10003/view/game/comp/CompGameMain.ts
@@ -488,7 +488,7 @@ export class CompGameMain extends FGUICompGameMain {
             });
         } else if (data.code == ROOM_END_FLAG.VOTE_DISBAND) {
             Logger.log("投票解散 " + msg);
-            this.changeToLobbyScene();
+            //this.changeToLobbyScene();
         }
     }
 
