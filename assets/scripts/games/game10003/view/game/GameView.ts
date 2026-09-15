6c66e6601db678cd34dd1dc62567f83615a79450
kakamatong <631603920@qq.com>
2026-09-15 13:35:38 +0800

    feat: 更新游戏视图，添加背景组件并调整加载包

--- assets/scripts/games/game10003/view/game/GameView.ts
+++ assets/scripts/games/game10003/view/game/GameView.ts
@@ -10,7 +10,7 @@ import * as fgui from "fairygui-cc";
  * 游戏视图 - 只处理背景显示
  */
 @ViewClass()
-@PackageLoad(["props", "gameCommon"])
+@PackageLoad(["lobbyBg", "props", "gameCommon"])
 export class GameView extends FGUIGameView {}
 
 fgui.UIObjectFactory.setExtension(GameView.URL, GameView);
